1525e10a471c0cc73d16df3726b54716d9088e06
Erty Seidohl <ertysdl+github@gmail.com>
2019-12-01 23:57:02 -0500

    Day one

---
+++ day1.hs
@@ -0,0 +1,28 @@
+fuel :: Int -> Int
+fuel mass = div mass 3 - 2
+
+fuel' :: Int -> Int
+fuel' f = fuelcalc (0, f)
+
+fuelcalc :: (Int, Int) -> Int
+fuelcalc (s, c) = do
+    let nx = fuel c
+    if nx <= 0
+        then s
+        else fuelcalc (s + nx, nx)
+
+
+getRawInput :: IO String
+getRawInput = readFile "./day1.input"
+
+getModules :: String -> [Int]
+getModules raw = do
+    let l = lines raw
+    map read l :: [Int]
+
+main :: IO ()
+main = do
+    raw <- getRawInput
+    let modules = getModules raw
+    let total = sum $ map fuel' modules
+    print . show $ total
