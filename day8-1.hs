82bab9c9faab4a936287b9c03e094980b36df364
Erty Seidohl <ertysdl+github@gmail.com>
2019-12-22 16:25:42 -0600

    Day 8

---
+++ day8-1.hs
@@ -0,0 +1,48 @@
+import Data.Char (isDigit)
+import Data.List (elemIndex)
+import Debug.Trace (trace)
+
+getRawInput :: IO String
+getRawInput = readFile "./day8.input"
+
+type Layer = [[Int]]
+
+splitLayers :: Int -> Int -> String -> [Layer]
+splitLayers width height input =
+    splitLayers' width height [read [c] :: Int | c <- input, isDigit c]
+
+splitLayers' :: Int -> Int -> [Int] -> [Layer]
+splitLayers' width height input
+    | null input = []
+    | otherwise =
+        let
+            (curr,next) = splitAt (width * height) input
+        in
+            toLayer width height curr : splitLayers' width height next
+
+toLayer :: Int -> Int -> [Int] -> Layer
+toLayer width height input
+    | null input = []
+    | otherwise =
+        let
+            (curr, next) = splitAt width input
+        in
+            curr : toLayer width height next
+
+numberOfDigits :: Int -> Layer -> Int
+numberOfDigits d layer =
+    let
+        filtered = map (filter (== d)) layer :: Layer
+        counted = map length filtered :: [Int]
+    in sum counted
+
+main :: IO ()
+main = do
+    rawInput <- getRawInput
+    let layers = splitLayers 25 6 rawInput
+    let numZeros = map (numberOfDigits 0) layers
+    let maxZeros = minimum numZeros
+    let total = case elemIndex maxZeros numZeros of
+                    Nothing -> error "no max zeros layer?"
+                    Just i -> product [numberOfDigits 1 (layers !! i), numberOfDigits 2 (layers !! i)]
+    print total
---
+++ day8-2.hs
@@ -0,0 +1,55 @@
+import Data.Char (isDigit)
+
+getRawInput :: IO String
+getRawInput = readFile "./day8.input"
+
+type Layer = [[Int]]
+
+splitLayers :: Int -> Int -> String -> [Layer]
+splitLayers width height input =
+    splitLayers' width height [read [c] :: Int | c <- input, isDigit c]
+
+splitLayers' :: Int -> Int -> [Int] -> [Layer]
+splitLayers' width height input
+    | null input = []
+    | otherwise =
+        let
+            (curr,next) = splitAt (width * height) input
+        in
+            toLayer width height curr : splitLayers' width height next
+
+toLayer :: Int -> Int -> [Int] -> Layer
+toLayer width height input
+    | null input = []
+    | otherwise =
+        let
+            (curr, next) = splitAt width input
+        in
+            curr : toLayer width height next
+
+combinePixel :: Int -> Int -> Int
+combinePixel 2 x = x
+combinePixel x _ = x
+
+combineRow :: [Int] -> [Int] -> [Int]
+combineRow = zipWith combinePixel
+
+combineLayers :: Layer -> Layer -> Layer
+combineLayers = zipWith combineRow
+
+combineImage :: [Layer] -> Layer
+combineImage = foldl1 combineLayers
+
+showImage :: Layer -> IO ()
+showImage [] = print ""
+showImage (r:rs) = do
+    print $ map (\x -> if x == 1 then "#" else ".") r
+    showImage rs
+
+main :: IO ()
+main = do
+    rawInput <- getRawInput
+    let layers = splitLayers 25 6 rawInput
+    print layers
+    let image = combineImage layers
+    showImage image
