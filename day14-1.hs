e4a049fe717b5de59d1e2f1a52d4ca01db6971a8
Erty Seidohl <ertysdl+github@gmail.com>
2019-12-27 01:48:12 -0600

    Day 14 store

--- day14-1.hs
+++ day14-1.hs
@@ -1,12 +1,17 @@
+-- Actual sol'n in day14-1b.hs
+
 -- import Debug.Trace (trace)
 import Data.Char (isDigit)
+import qualified Data.Map.Lazy as Map
+import Data.Map.Lazy (Map)
 
 getRawInput :: IO String
 getRawInput = readFile "./day14.input"
 
 data Chem = Chem {amt :: Int, name :: String} deriving (Show, Eq)
 data Reaction = Reaction {uses :: [Chem], gives :: Chem}
-data RNode = RNode {chem :: Chem, children :: [RNode]} deriving (Show)
+
+type Store = Map String Int
 
 split :: Char -> String -> [String]
 split _ "" = []
@@ -33,30 +38,52 @@ toReaction s =
 produces :: String -> Reaction -> Bool
 produces s = (== s) .  name . gives
 
+multr :: Int -> Reaction -> Reaction
+multr m r =
+    r {
+        uses = map (multc m) (uses r)
+    }
+
+multc :: Int -> Chem -> Chem
+multc m c =
+    c {
+        amt = amt c * m
+    }
+
 findProducer :: [Reaction] -> Chem -> Reaction
 findProducer rs c
     | name c == "ORE" = Reaction { uses = [], gives = c }
-    | otherwise = head $ filter (produces $ name c) rs
+    | otherwise =
+        let
+            reaction = head $ filter (produces $ name c) rs
+            multiplier = max 1 $ amt c `div` amt (gives reaction)
+        in
+            multr multiplier reaction
 
-toRNode :: [Reaction] -> Reaction -> RNode
-toRNode rs r =
-    RNode {
-        chem = gives r
-        , children = map (toRNode rs . findProducer rs) (uses r)
-    }
+givesOre :: Reaction -> Bool
+givesOre r = (== "ORE") $ name $ gives r
 
--- rollUpOre :: RNode -> Int -> Int
--- rollUpOre rn mult
---     | name (chem rn) == "ORE" = mult * amt (chem rn)
---     | otherwise = rollUpOre
+rollUpOre :: [Reaction] -> Reaction -> Int
+rollUpOre rs r = rollUpOre' rs [r] 0 (Map.empty :: Map String Int)
+
+expand :: Reaction -> ([Reaction] -> Store)
+expand r =
+
+rollUpOre' :: [Reaction] -> [Reaction] -> Int -> Store -> Int
+rollUpOre' _ [] orecount _ = orecount
+rollUpOre' reactions (curr:rest) orecount store
+    | givesOre curr = rollUpOre' reactions rest (orecount + amt (gives curr)) store
+    | otherwise =
+        let
+            (expanded, newstore) = expand curr store
+        in
+            rollUpOre' reactions (rest ++ expanded) orecount newstore
 
 main :: IO ()
 main = do
     input <- getRawInput
     let reactions = map toReaction $ lines input
     let fuelReaction = head $ filter (produces "FUEL") reactions
-    let reactionTree = toRNode reactions fuelReaction
-    print reactionTree
-    -- let result = rollUpOre reactionTree 1
-    -- print result
+    let result = rollUpOre reactions fuelReaction
+    print result
 
